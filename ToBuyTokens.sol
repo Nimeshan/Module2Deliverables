aa2c8787ade8ebdd9aa5356a9cc97aed9e6391db
Nilupul Weerasinghe <nilupulweerasinghe27@gmail.com>
2025-02-27 20:04:13 +0530

    Tokens to buy Kitty NFT

---
+++ ToBuyTokens.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyERC20Token is ERC20, Ownable {
+    uint256 public constant INITIAL_SUPPLY = 1_000_000 * 10**18; // 1 million tokens
+
+    constructor() ERC20("KittyBuyTokens", "KIT") Ownable(msg.sender) {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
